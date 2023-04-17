62e5291c11753e553850cbcff2d7f5399897c386
yaguch1 <128358148+yaguch1@users.noreply.github.com>
2023-04-17 14:57:30 +0900

    Revert "PAY.JPを用いたクレジットカードでの決済機能"
    This reverts commit 38f05378774d223db77f9c63f9c65ec37054d36c.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,7 +16,6 @@ class OrdersController < ApplicationController
     @order_address = OrderAddress.new(order_params)
 
     if @order_address.valid?
-      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -27,7 +26,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 
   def move_to_index
@@ -36,13 +35,5 @@ class OrdersController < ApplicationController
       redirect_to root_path
     end
   end
-
-  def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
-      amount: @item.price,  # 商品の値段
-      card: order_params[:token],    # カードトークン
-      currency: 'jpy'                 # 通貨の種類（日本円）
-    )
-  end
+   
 end
